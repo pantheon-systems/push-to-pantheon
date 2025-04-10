625420bd55b039cd9d58824ef66987d6b01fe8fe
Steve Persch <stevector@users.noreply.github.com>
2025-04-10 20:29:31 +0000

    uisng script again

--- scripts/prepare-repo.sh
+++ scripts/prepare-repo.sh
@@ -7,9 +7,10 @@ set -euo pipefail
 # $PANTHEON_REPO_LOCATION
 # $PANTHEON_TARGET_ENV
 
+
+
 git remote add pantheon $PANTHEON_REPO_LOCATION
 git remote -v
-
 
 if git ls-remote --exit-code --heads pantheon "$PANTHEON_TARGET_ENV" > /dev/null; then
     echo "the branch already exists in the remote"
@@ -17,13 +18,14 @@ else
   git fetch pantheon master
   git push pantheon FETCH_HEAD:refs/heads/$PANTHEON_TARGET_ENV
 fi
-
 git fetch pantheon $PANTHEON_TARGET_ENV
+
 # Reset your working directory to match the remote branch
 git reset --hard pantheon/$PANTHEON_TARGET_ENV
 # Create and switch to a local branch tracking the remote one
 # todo, name the branch based on some variable.
 git checkout -B temp-build-branch
+
 
 # todo, wrap this in a check for whether the git committing and pushing should be done directly like this, or by
 # terminus -n build:env:create
